3af7fba798a11ec7af28d4750450f5188290455e
Prashant Thakur <honey.2609@gmail.com>
2019-03-09 17:23:32 +0530

    Added Forgot password test

---
+++ src/test/java/ForgotPasswordTest.java
@@ -0,0 +1,32 @@
+import okhttp3.Response;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import util.HttpUtil;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ForgotPasswordTest {
+    @Test
+    public void testForgotPassword() throws IOException {
+        // Given
+        String url = "https://api.automate.io/auth/forgotPassword";
+        JSONObject jsonObject = new JSONObject();
+        String testUserEmail = "kushal.da@sync.io";
+        jsonObject.put("email", testUserEmail);
+
+        // When
+        Response response = HttpUtil.post(url, jsonObject);
+
+        // Then
+        assertThat(response).isNotNull();
+        assertThat(response.code()).isNotEqualTo(HttpURLConnection.HTTP_NOT_FOUND);
+        assertThat(response.code()).isEqualTo(HttpURLConnection.HTTP_OK);
+        JSONObject responseBody = new JSONObject(response.body().string());
+        assertThat(responseBody.get("status")).isEqualTo("success");
+        assertThat(responseBody.getJSONObject("data").get("message"))
+                .isEqualTo("Password reset email sent. Please read instructions in the email to proceed futher.");
+    }
+}
